70edf75e36ca3c8191ae5fe903ed1bbf8d016e31
Jeyanthi Kannan <jeyanthikannan2005@gmail.com>
2026-03-04 10:57:22 +0530

    Traffic Signal System using if Statement

---
+++ 4.q1/TrafficSignal.java
@@ -0,0 +1,19 @@
+class TrafficSignal
+{
+	public static void main(String[] args)
+	{
+		char code='R';
+		if(code=='R')
+		{
+			System.out.println("STOP");
+		}
+		if(code=='Y')
+		{
+			System.out.println("READY");
+		}
+		if(code=='G')
+		{
+			System.out.println("GO");
+		}
+	}
+}
